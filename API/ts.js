7ae0a6b69dbec43c11b1626729ddf662fda0fa4f
darkneslord19 <guvenkaratas.2025@gmail.com>
2026-06-19 01:20:15 +0300

    Refactor API handler for URL fetching

--- API/ts.js
+++ API/ts.js
@@ -1,16 +1,17 @@
+module.exports = async (req, res) => {
+  const url = req.query.url;
+  
+  if (!url) {
+    return res.status(400).send('URL gerekli');
+  }
 
-// api/ts.js
-module.exports = async (req, rrres) => {
-    const url = req.query.url;
-        if (!url) {
-              return res.status(400).send('URL gerekli');
-        }
-
-    try {
-          const response = await fetch(decodeURIComponent(url));    const buffer = await response.arrayBuffer();
-
-      res.setHeader('Content-Type', 'video/mp2t');
-          res.status(200).send(Buffer.from(buffer));
-    } catch (error) {
-           res.status(500).send('Segment hatas');;;
-    }};
+  try {
+    const response = await fetch(decodeURIComponent(url));
+    const buffer = await response.arrayBuffer();
+    
+    res.setHeader('Content-Type', 'video/mp2t');
+    res.status(200).send(Buffer.from(buffer));
+  } catch (error) {
+    res.status(500).send('Segment hatası');
+  }
+};
